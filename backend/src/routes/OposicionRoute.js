75bf0ad165db0f7a0d421890fe2aac1df14085b9
Rafael Javier Luengo Barreno <010.rafaeljavier.luengobarreno@gmail.com>
2026-04-12 15:27:36 +0200

    Subida de Backend, BBDD y Documentacion completa. Falta front por ajustes y pruebas. Repositorio y GitHub exclusivo para la presentación del proyecto TFG

--- backend/src/routes/OposicionRoute.js
+++ backend/src/routes/OposicionRoute.js
@@ -4,7 +4,6 @@ const oposicionesController = require("../controllers/OposicionController");
 const {
   validarToken
 } = require('../middleware/authMiddleware');
-// Express 5 / path-to-regexp ya no admite /:id(\d+) en la ruta; validamos el id aquí.
 function soloIdNumerico(req, res, next) {
   const {
     id
@@ -17,7 +16,6 @@ function soloIdNumerico(req, res, next) {
   }
   next();
 }
-// Raíz con y sin barra final.
 router.get(["/", ""], oposicionesController.getOposiciones);
 router.get("/rss/:id", validarToken, oposicionesController.getNoticiasRss);
 router.get("/requisitos/:id/:genero", validarToken, oposicionesController.getRequisitos);
