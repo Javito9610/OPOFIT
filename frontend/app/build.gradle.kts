b324a25461277f9e3e95975700e46588027a909c
Rafael Javier Luengo Barreno <010.rafaeljavier.luengobarreno@gmail.com>
2026-06-14 21:56:53 +0200

    Fix missing Firebase config

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.gms.google-services")
+    // id("com.google.gms.google-services")  // descomenta cuando tengas google-services.json
 }
 
 android {
